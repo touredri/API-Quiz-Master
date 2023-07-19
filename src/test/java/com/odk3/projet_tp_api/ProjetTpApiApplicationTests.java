c89bb6fd8f9ff8956c107e67dd7bb3a5cb38cdb5
SyTechSy <sydiakaridia38@gmail.com>
2023-07-19 19:00:45 +0000

    premier commit

---
+++ src/test/java/com/odk3/projet_tp_api/ProjetTpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odk3.projet_tp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
