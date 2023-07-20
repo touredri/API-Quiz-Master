4239007b0e6dbe4ffbb554d3e2e90a169afc53c7
adama konake <adamakoanke@gmail.com>
2023-07-20 19:47:55 +0000

    ajout de controller

---
+++ src/main/java/com/odk3/projet_tp_api/Controller/ReponseController.java
@@ -0,0 +1,7 @@
+package com.odk3.projet_tp_api.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ReponseController {
+}
---
+++ src/main/java/com/odk3/projet_tp_api/Service/ParticiperService.java
@@ -0,0 +1,7 @@
+package com.odk3.projet_tp_api.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParticiperService {
+}
