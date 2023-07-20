c945f28dbd3a50753babbb452196339d2c6724f4
adama konake <adamakoanke@gmail.com>
2023-07-20 18:50:28 +0000

    modif user reposi

--- src/main/java/com/odk3/projet_tp_api/Repository/UtilisateurRepository.java
+++ src/main/java/com/odk3/projet_tp_api/Repository/UtilisateurRepository.java
@@ -2,6 +2,8 @@ package com.odk3.projet_tp_api.Repository;
 
 import com.odk3.projet_tp_api.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur,Integer> {
 }
