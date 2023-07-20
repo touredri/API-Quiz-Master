5f18c5e49d52e9fd1a4c5dc4425b4a023889fde9
SyTechSy <sydiakaridia38@gmail.com>
2023-07-20 18:19:27 +0000

    Diakaridia Modification de la relation

--- src/main/java/com/odk3/projet_tp_api/model/Utilisateur.java
+++ src/main/java/com/odk3/projet_tp_api/model/Utilisateur.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Data
 @Entity
