5202f9fe93c77114e4180cdb031f5599f8c8a863
SyTechSy <sydiakaridia38@gmail.com>
2023-07-21 09:15:43 +0000

    Change de l'anotation controller

--- src/main/java/com/odk3/projet_tp_api/Controller/ParticiperController.java
+++ src/main/java/com/odk3/projet_tp_api/Controller/ParticiperController.java
@@ -1,7 +1,8 @@
 package com.odk3.projet_tp_api.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ParticiperController {
 }
