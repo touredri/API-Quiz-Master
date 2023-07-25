44f62614def984dca8dc2084b425731449cdc12a
unknown <sy.diakaridia@odc.local>
2023-07-25 16:02:59 +0000

    doccumentation quiz participer question

--- src/main/java/com/odk3/projet_tp_api/Controller/QuestionController.java
+++ src/main/java/com/odk3/projet_tp_api/Controller/QuestionController.java
@@ -3,7 +3,13 @@ package com.odk3.projet_tp_api.Controller;
 import com.odk3.projet_tp_api.Repository.QuestionRepository;
 import com.odk3.projet_tp_api.Service.QuestionService;
 import com.odk3.projet_tp_api.model.Question;
+import com.odk3.projet_tp_api.model.Quiz;
 import com.odk3.projet_tp_api.model.Utilisateur;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +26,19 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
 
+
+    // =====================================================================================================
+
+    @Operation(summary = "Ajouter des question ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Question ajouter",content = {
+                    @Content(mediaType = "application/json",schema = @Schema(implementation = Question.class))
+            }),
+            @ApiResponse(responseCode = "400",description = "Mauvaise requete", content = @Content),
+            @ApiResponse(responseCode = "409",description = "Qustion existe déjà", content = @Content),
+            @ApiResponse(responseCode = "500",description = "Erreur server", content = @Content)
+    })
+
     @PostMapping("/ajouter")
     public ResponseEntity<Object> ajouterQuestion(@RequestBody Question question){
         Question questionVerif = questionService.createQuestion(question);
@@ -28,6 +47,20 @@ public class QuestionController {
         }else
             return new ResponseEntity<>("Question existe déjà", HttpStatus.OK);
     }
+
+
+
+    // =====================================================================================================
+
+    @Operation(summary = "Modifier une Question ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Question modifier",content = {
+                    @Content(mediaType = "application/json",schema = @Schema(implementation = Question.class))
+            }),
+            @ApiResponse(responseCode = "400",description = "Mauvaise requete", content = @Content),
+            @ApiResponse(responseCode = "404",description = "introuvable", content = @Content),
+            @ApiResponse(responseCode = "500",description = "Erreur server", content = @Content)
+    })
 
     @PutMapping("/modifier")
     public ResponseEntity<Object> modifierQuestion(@RequestBody Question question){
@@ -39,10 +72,38 @@ public class QuestionController {
             return new ResponseEntity<>("Question n'existe pas", HttpStatus.OK);
     }
 
+
+
+    // =====================================================================================================
+
+    @Operation(summary = "List des Questions ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Question list",content = {
+                    @Content(mediaType = "application/json",schema = @Schema(implementation = Question.class))
+            }),
+            @ApiResponse(responseCode = "400",description = "Mauvaise requete", content = @Content),
+            @ApiResponse(responseCode = "204",description = "List vite", content = @Content),
+            @ApiResponse(responseCode = "500",description = "Erreur server", content = @Content)
+    })
+
     @GetMapping("list")
     public List<Question> listQuestion(){
         return questionService.getAllQuestions();
     }
+
+
+
+    // =====================================================================================================
+
+    @Operation(summary = "Supprimer une question ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Question supprimer",content = {
+                    @Content(mediaType = "text/plain",schema = @Schema(implementation = Question.class))
+            }),
+            @ApiResponse(responseCode = "400",description = "Mauvaise requete", content = @Content),
+            @ApiResponse(responseCode = "404",description = "Question existe pas", content = @Content),
+            @ApiResponse(responseCode = "500",description = "Erreur server", content = @Content)
+    })
 
     @DeleteMapping("/supprimer")
     public ResponseEntity<String> supprimerQuestion(@RequestBody Question question){
